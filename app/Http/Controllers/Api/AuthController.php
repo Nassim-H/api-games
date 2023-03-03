018e8a1dfdded4b675ba33b7a0c22019354c7c4d
Dorian Mouille <dorian_mouille@ens.univ-artois.fr>
2023-03-03 11:26:57 +0100

    La requête de déconnexion d'un adhérent #3

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -37,4 +37,13 @@ class AuthController extends Controller
             ]
         ]);
     }
+
+    public function logout() {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
 }
