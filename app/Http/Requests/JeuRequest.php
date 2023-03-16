1a886a094e0d6249c4c8cdeeeef8f2edb2c62a61
Nassim <101125927+Nassim-H@users.noreply.github.com>
2023-03-16 09:38:53 +0100

    jeu controller

---
+++ app/Http/Requests/JeuRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class JeuRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules() {
+    return [
+        'nom' => "required|string|between:5,50",
+        'description' => "required|string|between:5,500",
+        'age_min' => "required|numeric|between:5,65",
+        'nombre_joueurs_min' => "required|numeric|between:1,100",
+        'nombre_joueurs_max' => "required|numeric|between:1,100",
+        'duree_partie' => "required|numeric|between:5,65",
+        'categorie' => "required|string|between:5,50",
+        'theme' => "required|string|between:5,50",
+        'editeur' => "required|string|between:5,50",
+    ];
+}
+    public function messages(): array {
+        return [
+            'required' => 'Le champ :attribute est obligatoire',
+            'between' => 'Le champ :attribute doit contenir entre :min et :max caractéres.',
+            'age.between' => 'L\'age minimum doit avoir une valeur comprise entre :min et :max.',
+            'nombre_joueurs_min.between' => 'Le nombre de joueurs minimum doit avoir une valeur comprise entre :min et :max.',
+            'nombre_joueurs_max.between' => 'Le nombre de joueurs maximum doit avoir une valeur comprise entre :min et :max.',
+        ];
+ }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\JeuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('jeux', JeuController::class);
+
+
+Route::prefix('jeux')->group(function () {
+    Route::get('/', [JeuController::class, 'index'])
+        ->name('jeux.index ');
+    Route::get('/{id}', [JeuController::class, 'show'])
+        ->middleware(['auth', 'role:view-salle'])
+        ->name('jeux.show');
+    Route::put('/', [JeuController::class, 'store'])
+        ->middleware(['auth', 'role:edit-salle'])
+        ->name('jeux.update');
+    Route::post('/{id}', [JeuController::class, 'update'])
+        ->name('jeux.store');
+    Route::delete('/{id}', [JeuController::class, 'destroy'])
+        ->middleware(['auth', 'role:admin'])
+        ->name('jeux.destroy');
 });
