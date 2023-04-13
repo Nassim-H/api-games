cad0e03ea25b84693983795b5cffde797696607f
Nassim <101125927+Nassim-H@users.noreply.github.com>
2023-04-13 11:27:33 +0200

    Ajout d'une route et d'une méthode affichant le prix moyen d'un jeu

--- app/Http/Controllers/Api/JeuController.php
+++ app/Http/Controllers/Api/JeuController.php
@@ -23,6 +23,92 @@ class JeuController extends Controller
 {
 
     /**
+     * @OA\Get(
+     *     path="/jeux",
+     *     summary="Afficher une liste de jeux",
+     *     tags={"Jeux"},
+     *     @OA\Parameter(
+     *         name="nb_joueurs_min",
+     *         in="query",
+     *         description="Nombre minimum de joueurs",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int32"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="nb_joueurs_max",
+     *         in="query",
+     *         description="Nombre maximum de joueurs",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int32"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="duree",
+     *         in="query",
+     *         description="Durée de la partie",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int32"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="age",
+     *         in="query",
+     *         description="Âge minimum",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int32"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="editeur",
+     *         in="query",
+     *         description="ID de l'éditeur",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int32"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="theme",
+     *         in="query",
+     *         description="ID du thème",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int32"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="categorie",
+     *         in="query",
+     *         description="ID de la catégorie",
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int32"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sort",
+     *         in="query",
+     *         description="Tri par nom (asc ou desc)",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Renvoie une liste de jeux",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/JeuResource")
+     *         )
+     *     ),
+     *     security={{"bearerAuth":{}}}
+     * )
+    /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
@@ -41,34 +127,31 @@ class JeuController extends Controller
         if ($nb_joueurs_min) {
             // Effectuer une requête en utilisant la valeur du paramètre
             $jeux = Jeu::where('nombre_joueurs_min', '=', $nb_joueurs_min)->get();
-        } else if ($nb_joueurs_max) {
+        } if ($nb_joueurs_max) {
             // Si le paramètre n'est pas présent, obtenir tous les jeux
             $jeux = Jeu::where('nombre_joueurs_max', '=', $nb_joueurs_max)->get();
-        } else if ($duree) {
-            // Si le paramètre n'est pas présent, obtenir tous les jeux
-            $jeux = Jeu::where('duree_partie', '=', $duree)->get();
-        } else if ($age) {
+        }  if ($age) {
             // Si le paramètre n'est pas présent, obtenir tous les jeux
             $jeux = Jeu::where('age_min', '=', $age)->get();
-        } else if ($editeur) {
+        }
+        if ($duree) {
+            // Si le paramètre n'est pas présent, obtenir tous les jeux
+            $jeux = Jeu::where('duree_partie', '=', $duree)->get();
+        }  if ($editeur) {
             // Si le paramètre n'est pas présent, obtenir tous les jeux
             $jeux = Jeu::where('editeur_id', '=', $editeur)->get();
-        } else if ($theme) {
+        }  if ($theme) {
             // Si le paramètre n'est pas présent, obtenir tous les jeux
             $jeux = Jeu::where('theme_id', '=', $theme)->get();
-        } else if ($categorie) {
+        }  if ($categorie) {
             // Si le paramètre n'est pas présent, obtenir tous les jeux
             $jeux = Jeu::where('categorie_id', '=', $categorie)->get();
-        } else if ($sort) {
+        }  if ($sort) {
             // Si le paramètre n'est pas présent, obtenir tous les jeux
             $jeux = Jeu::orderBy('nom', $sort)->get();
         }
 
 
-        else {
-            // Si le paramètre n'est pas présent, obtenir tous les jeux
-            $jeux = Jeu::all();
-        }
 
         // Retourner la collection de jeux en utilisant la ressource JeuResource
         return JeuResource::collection($jeux->all());
@@ -211,5 +294,15 @@ class JeuController extends Controller
             "note_moyenne" => $jeu->commentaires->avg('note')
         ], 200);
 
+    }
+
+    public function prix_moyen($id){
+        $jeu = Jeu::findOrFail($id);
+        $prix_moyen = $jeu->achats->avg('prix');
+        return response()->json([
+            "status"=> "success",
+            "message"=>"Prix moyen",
+            "prix_moyen"=> $prix_moyen,
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,7 @@ Route::prefix('jeux')->group(function () {
     Route::get('/jeux', [JeuController::class, 'index'])
         ->middleware('auth')
         ->name('jeux.index ')
-        ->where(['age' =>'[0-9]+', 'duree'=>'[0-9]+', 'nb_joueurs_min'=>'[0-9]+', 'nb_joueurs_max'=>'[0-9]+', 'sort'=>'(asc|desc)', 'categorie'=>'[0-9]+', 'theme'=>'[a-zA-Z0-9]+', 'editeur'=>'[0-9]+']);
+        ->where(['age' =>'[0-9]+', 'duree'=>'[0-9]+', 'nb_joueurs_min'=>'[0-9]+', 'nb_joueurs_max'=>'[0-9]+', 'sort'=>'(asc|desc)', 'categorie'=>'[a-zA-Z0-9]+', 'theme'=>'[a-zA-Z0-9]+', 'editeur'=>'[a-zA-Z0-9]+']);
     Route::post('/jeux', [JeuController::class, 'store'])
         ->middleware(['essaie:adherent-premium,commentaire-moderateur,administrateur'])
         ->name('jeux.store');
@@ -73,6 +73,10 @@ Route::prefix('jeux')->group(function () {
     Route::get('/{id}', [JeuController::class, 'details'])
         ->middleware(['auth'])
         ->name('jeux.details');
+
+    Route::get('/prix_achat_moyen/{id}', [JeuController::class, 'prix_moyen'])
+        ->middleware(['auth'])
+        ->name('jeux.prix_moyen');
 });
 
 
